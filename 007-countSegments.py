1e2317f96b4f5a3bf1a1b5bf621be2de20397fbe
Jiaozn <jiaozn@163.com>
2020-11-10 14:32:19 +0800

    007

---
+++ 007-countSegments.py
@@ -0,0 +1,14 @@
+class Soloution:
+    def countSegments(self, s):
+        res = 0
+        for i in range(len(s)):
+            if s[i] != ' ' and (i == 0 or s[i - 1] == ' '):
+                res += 1
+        return res
+
+
+if __name__ == "__main__":
+    s = Soloution()
+    n = "Hello, My name is Jiaozn"
+    print("输入：", n)
+    print("输出：", s.countSegments(n))
