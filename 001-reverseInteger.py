88dcca5d5e5131854486b571969d2dd0030dda4e
Jiaozn <jiaozn@163.com>
2020-11-10 14:11:49 +0800

    007

---
+++ 001-reverseInteger.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseIntrger(self, number):
+        h = int(number/100)
+        t = int(number % 100/10)
+        z = int(number % 10)
+        return(100 * z + 10 * t + h)
+
+if __name__ == '__main__':
+    solution = Solution()
+    num = 123
+    ans = solution.reverseIntrger(num)
+    print("输入：", num)
+    print("输出：", ans)
